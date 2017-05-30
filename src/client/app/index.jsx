15392c35cb04012bd7080118a4ffc7166f23a6ee
shawkes <shawkes@gmail.com>
2017-05-30 20:11:48 +0100

    Basic React Routing

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,26 +1,44 @@
-if (module.hot) {
- module.hot.accept()
-}
 import React from 'react'
-import {render} from 'react-dom'
+import { render } from 'react-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom'
+
 require('./index.css')
-var CpeSearch = require('./components/CpeSearch.jsx');
 
+const CpeSearch = require('./components/CpeSearch.jsx')
 
-class App extends React.Component {
-  render() {
-    const handleSubmit = function() {}
-    return (
-      <div>
-        Genie!
-        <CpeSearch
-        />
-      </div>
-    )
-  }
+if (module.hot) {
+  module.hot.accept()
 }
+
+const Tasks = () => (
+  <div>
+    <h2>tasks</h2>
+  </div>
+)
+
+const App = () => (
+  <Router>
+    <div>
+      <h1>Genie!</h1>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/tasks">Tasks</Link></li>
+      </ul>
+
+      <hr />
+
+      <Route exact path="/" component={CpeSearch} />
+      <Route path="/tasks" component={Tasks} />
+    </div>
+  </Router>
+)
+
 
 render(
-  <App/>,
-  document.getElementById('app')
+  <App />,
+  document.getElementById('app'),
 )
